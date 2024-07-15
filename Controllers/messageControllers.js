0b8949bd289724a9d9d9c4c264e4e193029f311b
Samhitha1212 <samhithadondeti@gmail.com>
2024-07-15 15:46:47 +0530

    ..

--- Controllers/messageControllers.js
+++ Controllers/messageControllers.js
@@ -10,6 +10,7 @@ const message=req.body
 const {chatid}=req.params
 const newMessage= new messageModel(message)
 const savedmessage=await newMessage.save()
+console.log("before socket emit")
 const updatedChat=await chatModel.findByIdAndUpdate(chatid,{
   $push:{messages:savedmessage._id},
   lastMessage:savedmessage._id
@@ -19,6 +20,7 @@ const updatedChat=await chatModel.findByIdAndUpdate(chatid,{
   //   return (member != message.author)
   // })
 )
+console.log("before socket emit")
   if(receiversSocketId.length >0){
     receiversSocketId.forEach((Id)=>{
       io.to(Id).emit("newMessage",{
@@ -35,7 +37,7 @@ const updatedChat=await chatModel.findByIdAndUpdate(chatid,{
 
 
 const getChatMessages=async(req,res)=>{
-
+  console.log("before socket emit")
   const {chatid}=req.params
   const chat= await Chat.findById(chatid).then(chat=>{
     messageModel.find({_id: {$in: chat.messages}}).then(data=>{
