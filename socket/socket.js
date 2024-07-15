e74d57178d078ff9e5254911259537bc46bbdbd3
Samhitha1212 <samhithadondeti@gmail.com>
2024-07-15 14:35:38 +0530

    Chanded frontendurl

--- socket/socket.js
+++ socket/socket.js
@@ -8,7 +8,7 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
   cors:{
-    origin:'https://livechat-frontend-w5cx.onrender.com',
+    origin:'https://chatapp1212.netlify.app/',
     methods: ["GET","POST","PUT","DELETE"],
   }
 })
